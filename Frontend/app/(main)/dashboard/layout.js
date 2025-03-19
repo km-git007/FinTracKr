80b76b49135d3c13661297a50590ebbe87c50939
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-03-20 05:06:41 +0530

    Update dashboard layout

--- Frontend/app/(main)/dashboard/layout.js
+++ Frontend/app/(main)/dashboard/layout.js
@@ -13,6 +13,12 @@ export default function Layout() {
                Dashboard
             </h1>
          </div>
+      <div className="flex items-center justify-between mb-5">
+            <h1 className="text-6xl font-bold tracking-tight gradient-title-16 animate-gradient">
+               Account Details
+            </h1>
+            <Account/>
+         </div>
          <Suspense
             fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
          >
